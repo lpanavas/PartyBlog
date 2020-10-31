9e63149868c5d60688ab3595834073a3ef7aabde
lpanavas <71109392+lpanavas@users.noreply.github.com>
2020-10-31 14:09:49 -0400

    deleted in index.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+
 import { useHistory } from "react-router-dom";
 function Landing() {
   const history = useHistory();
